132fae5b52107ea32aa3651d3a348103cd262154
Patrick Steiger <psteiger@gmail.com>
2016-05-06 02:08:24 -0300

    dir_fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-coffee --compile --output assets/javascript/
+coffee --compile --output assets/javascript/ src/animate-on-scroll.coffee
 git add .
 git commit -a -m "$1"
 npm version patch
